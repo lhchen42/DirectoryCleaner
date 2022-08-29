330c895e6c5562f53c2d85db4676d793483f711b
Lin Chen <32826973+lhchen42@users.noreply.github.com>
2022-08-29 19:04:54 -0400

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import shutil
+from config import *
+
+def main():
+    print("Hello World")
+    print(ROOT)
+    print(SITES)
+        
+
+if __name__ == "__main__":
+    main()
---
+++ unittest/test_main.py
@@ -0,0 +1,8 @@
+from unittest import main, TestCase
+from main import main
+
+#Test unit for validations functions 
+
+class TestMain(TestCase):
+    def test_main():
+        pass
