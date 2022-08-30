f50d4bf340ba404b21545b330b6b27fedbf68204
Lin Chen <32826973+lhchen42@users.noreply.github.com>
2022-08-30 09:32:15 -0400

    v1

--- main.py
+++ main.py
@@ -3,11 +3,33 @@ import sys
 import shutil
 from config import *
 
+import logging
+
+logging.basicConfig(filename="debug.log")
+# sites
+# tvmarkets, latam, tvglobal, tvforex
+
 def main():
-    print("Hello World")
-    print(ROOT)
-    print(SITES)
-        
+    print("ROOT: {}".format(ROOT))
+    print("SITES: {}".format(SITES))
+    try:
+        for site in SITES:
+            site_path = os.path.join(ROOT, site)
+            # loop through dir in the site directory        
+            dirs = os.listdir(site_path)
+            for dir in dirs:
+                target_dir = os.path.join(site_path, dir)
+                sys.stdout.write("Remove {}".format(target_dir))
+                # remove directory or file
+                if os.path.isdir(target_dir):
+                    shutil.rmtree(target_dir)
+                else:
+                    os.remove(target_dir)
+                sys.stdout.flush()
+                sys.stdout.write("\r" + "Remove {} ........... Done!\n".format(target_dir))
+    except Exception as e:
+        logging.error("Error on removing directory: {}".format(e))
+
 
 if __name__ == "__main__":
     main()
